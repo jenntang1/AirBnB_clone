d519905e0020ba6f5c4bf64986e73e620b0aa515
jenntang1 <1039@holbertonschool.com>
2020-02-15 23:56:53 +0000

    [UPDATE] Remove constructor

--- models/amenity.py
+++ models/amenity.py
@@ -4,15 +4,9 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ Subclass that inherits from BaseModel """
-    def __init__(self, *args, **kwargs):
-        """ Implementing __init__ method as the constructor.
-        Args:
-            args: positional command line args
-            kwargs: keyword command line args
-        Attributes:
-            ----------- public class attributes -----------
-            name: empty string
-        """
-        super(Amenity, self).__init__(*args, **kwargs)
-        self.name = ""
+    """ Subclass that inherits from BaseModel
+    Attributes:
+        ----------- public class attributes -----------
+        name: empty string
+    """
+    self.name = ""
