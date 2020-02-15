db445e9b45fd9ed1255eef00acd2fe76871000fa
jenntang1 <1039@holbertonschool.com>
2020-02-15 15:03:54 +0000

    [UPDATE] Changed reload and save methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ FileStorage Implementation """
 import json
-import os
+from os import path
 
 
 class FileStorage:
@@ -14,32 +14,37 @@ class FileStorage:
                    an empty dictionary
     """
     __file_path = "file.json"
-    __objects
+    __objects = {}
 
     def all(self):
         """ Creating public instance method that returns
         the dictionary __objects.
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ Creating public instance method that sets in
         __objects the obj with key <obj class name>.id.
         """
-        self.__objects[obj.id] = obj
+        key = str(type(obj).__name__) + "." + str(obj)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ Creating public instance method that serializes
         __objects to the JSON file, __file_path.
         """
-        temp_dict = dict(self.__objects)
-        for key, value in temp_dict.items():
-            with open(self.__file_path, 'w') as save_json:
-                json.dump(temp_dict, save_json)
+        temp_dict = dict(FileStorage.__objects)
+        with open(FileStorage.__file_path, 'w') as save_json:
+            json.dump(temp_dict, save_json)
 
     def reload(self):
         """ Creating public instance method that deserializes
         the JSON file to __objects.
         """
-        if self.__file_path is not None:
-            json.load(self.__file_path)
+        if path.exists(FileStorage.__file_path):
+            from models.base_model import BaseModel
+            instance = {}
+            with open(FileStorage.__file_path, 'r') as from_json:
+                instance = json.load(from_json)
+                for key, value in instance.items():
+                    FileStorage.__objects = BaseModel(**value)
