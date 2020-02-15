64734518b20686fe1c3e2fecbf159ae49ac515b9
jenntang1 <1039@holbertonschool.com>
2020-02-15 15:03:03 +0000

    [UPDATE] Add storage variables

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self.to_dict())
+            storage.new(BaseModel)
 
     def __str__(self):
         """ Implementing __str__ method for string
