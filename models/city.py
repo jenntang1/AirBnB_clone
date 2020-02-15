2334b375f7ce020edc8867cd6e2119fb538f4466
jenntang1 <1039@holbertonschool.com>
2020-02-15 23:57:26 +0000

    [UPDATE] Remove constructor

--- models/city.py
+++ models/city.py
@@ -4,17 +4,11 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ Subclass that inherits from BaseModel """
-    def __init__(self, *args, **kwargs):
-        """ Implementing __init__ method as the constructor.
-        Args:
-            args: positional command line args
-            kwargs: keyword command line args
-        Attributes:
-            ----------- public class attributes -----------
-            state_id: empty string but will be State.id
-            name: empty string
-        """
-        super(City, self).__init__(*args, **kwargs)
-        self.state_id = kwargs.get("state_id")
-        self.name = ""
+    """ Subclass that inherits from BaseModel
+    Attributes:
+        ----------- public class attributes -----------
+        state_id: empty string but will be State.id
+        name: empty string
+    """
+    self.state_id = ""
+    self.name = ""
