ef3f4392e36ca0551a6e8ebfee0f80e14623e7e7
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-02-17 19:57:20 +0000

    [UPDATE] Account for assertIsInstance

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,20 +2,34 @@
 """ Unittest for BaseModel """
 from unittest import TestCase
 from models.base_model import BaseModel
-
+from uuid import UUID
+from datetime import datetime as dt
 
 class BaseModel_Test(unittest.TestCase):
     """ Creating class to test base class. """
+    def __init__(self, *args, **kwargs):
+        self.test_class = BaseModel
+        self.test_name = "BaseModel"
+
+    def test_id(self):
+        self.assertIsInstance(self.test_class.id, str)
+        self.assertIsInstance(self.test_class.id, UUID)
+
+    def test_create(self):
+        base = self.test_class
+        now = dt.now()
+        self.assertIsInstance(base.test_class.created_at, dt)
+
+    def test_update(self):
+        base = self.test_class
+        now = dt.now()
+        base.dt.now()
+        self.assertNotEqual(base.dt, now)
 
     def test_save(self):
         """ Checking that name is saved """
         test = BaseModel.save('Flavio')
         self.assertEqual(test, {'name': 'Flavio'})
-
-    def test_type(self):
-        """ Checking type """
-        test = BaseModel.to_dict()
-        self.assertIs(type(test), str)
 
 if __name__ == "__main__":
     unittest.main()
