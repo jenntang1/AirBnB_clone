a9b2dd3004a7bd01e6597f5f5a9260c8c27e3bdf
jenntang1 <1039@holbertonschool.com>
2020-02-17 21:44:10 +0000

    [UPDATE] Add test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
 """ Unittest for BaseModel """
-from unittest import TestCase
-from models.base_model import BaseModel
+import unittest
 from uuid import UUID
-from datetime import datetime as dt
+from datetime import datetime
+from models.base_model import BaseModel
+
 
 class BaseModel_Test(unittest.TestCase):
-    """ Creating class to test base class. """
+    """ Creating class to test base class """
+
     def __init__(self, *args, **kwargs):
+        """ Using __init__ method to initialize BaseModel """
         self.test_class = BaseModel
         self.test_name = "BaseModel"
 
     def test_id(self):
+        """ Check id type """
         self.assertIsInstance(self.test_class.id, str)
         self.assertIsInstance(self.test_class.id, UUID)
+        self.assertIsInstance(self.test_name.id, str)
+        self.assertIsInstance(self.test_name.id, UUID)
 
     def test_create(self):
+        """ Check created_at type """
         base = self.test_class
-        now = dt.now()
-        self.assertIsInstance(base.test_class.created_at, dt)
+        current = datetime.now()
+        self.assertIsInstance(base.created_at, datetime)
 
     def test_update(self):
+        """ Check updated_at type """
         base = self.test_class
-        now = dt.now()
-        base.dt.now()
-        self.assertNotEqual(base.dt, now)
-
-    def test_save(self):
-        """ Checking that name is saved """
-        test = BaseModel.save('Flavio')
-        self.assertEqual(test, {'name': 'Flavio'})
+        current = datetime.now()
+        update = base.datetime.now()
+        self.assertIsInstance(base.updated_at, datetime)
 
 if __name__ == "__main__":
     unittest.main()
