7d3b4d10e6b6d689474513e95d8e0925e1d49a95
jenntang1 <1039@holbertonschool.com>
2020-02-18 23:49:25 +0000

    [UPDATE] Pep8 compliance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,7 +48,7 @@ class FileStorage:
             from models.amenity import Amenity
             from models.city import City
             from models.place import Place
-            from models.review import Review 
+            from models.review import Review
             from models.state import State
             from models.user import User
             classes = {'BaseModel': BaseModel, 'User': User,
