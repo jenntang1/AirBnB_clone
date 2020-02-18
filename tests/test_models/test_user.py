c1426ac7b8626423b6989bab7a13fbb46b4e527d
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-02-18 20:38:03 +0000

    [UPDATE] Account for if attribute already set

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,11 +3,13 @@
 from unittest import TestCase
 from models.base_model import BaseModel
 from models.user import User
+from test.testmodels.test_base_model import TestBaseModel
 
 
-class User_Test(unittest.TestCase):
+class User_Test(TestBaseModel):
     """ Creating class to test User class. """
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs):
 
 if __name__ == "__main__":
     unittest.main()
