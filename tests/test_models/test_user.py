379753651f407079a1b4f6a37b6b4f0b8c5036b1
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-02-18 20:57:25 +0000

    [UPDATE] Add more test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,13 +3,30 @@
 from unittest import TestCase
 from models.base_model import BaseModel
 from models.user import User
-from test.testmodels.test_base_model import TestBaseModel
+from tests.test_models.test_base_model import BaseModel_Test
 
 
-class User_Test(TestBaseModel):
+class User_Test(BaseModel_Test):
     """ Creating class to test User class. """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs):
+        self.test_class = User
+        self.test_name = "User"
+
+    def test_user(self):
+        pass
+
+    def test_email(self):
+        pass
+
+    def test_password(self):
+        pass
+
+    def test_fist_name(self):
+        pass
+
+    def test_last_name(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
