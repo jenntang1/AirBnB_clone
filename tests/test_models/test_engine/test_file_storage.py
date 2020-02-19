eda0a87fe6dd35cc0edb74ac9605344a2696c443
jenntang1 <1039@holbertonschool.com>
2020-02-19 02:19:02 +0000

    [UPDATE] Add more test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,31 +1,62 @@
 #!/usr/bin/python3
-""" Unit tests for file storage """
-from unittest import TestCase
+""" Unittest for file storage """
+import os
+import unittest
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from test.testmodels.test_base_model import TestBaseModel
+from tests.test_models.test_base_model import BaseModel_Test
 
 
 class FileStorage_Test(unittest.TestCase):
     """ Creating class to test File Storage """
     def __init__(self, *args, **kwargs):
-        """ Using __init__ method to initialize FileStorage """
+        """ Using __init__ method """
         super().__init__(*args, **kwargs)
         self.test_class = FileStorage
-        self.test_name = "FileStorage"
 
     def test_file_path(self):
         """ Check file_path type """
-        test = self.test_file_path()
-        self.assertIsInstance(test.id, file)
-        pass
+        test = self.test_class()
+        self.assertIsInstance(test.__file_path, str)
+
+    def tearDown(self):
+        """ Using tearDown method """
+        try:
+            os.remove("file.json")
+        except:
+            pass
 
     def test_objects(self):
-        """ Check created_at type """
-        test = self.test_objects()
+        """ Check __objects type """
+        test = self.test_class()
         self.assertIsInstance(test.__objects, dict)
-        pass
 
-if __name__ == "__main__":
-    unittest.main()
+    def tearDown(self):
+        """ Using tearDown method """
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+    def test_empty(self):
+        """ Check empty file """
+        test = self.test_class()
+        self.assertEqual(test.__file_path, "")
+
+    def tearDown(self):
+        """ Using tearDown method """
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+    def test_None(self):
+        """ Check if there's no file """
+        test = self.test_class(None)
+        self.assertEqual(test.__file_path, "")
+
+    def tearDown(self):
+        """ Using tearDown method """
+        try:
+            os.remove("file.json")
+        except:
+            pass
